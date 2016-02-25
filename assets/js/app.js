096c563f0ff59ff39cc87902b5e6f780d69f3fd7
gregaubs <gaj.aubert@gmail.com>
2016-02-25 17:24:19 +0000

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,17 @@ $( document ).ready(function() {
 	});
 
 
+	window.addEventListener("load",function() {
+		if (isMobile) {
+			// Set a timeout...
+			setTimeout(function(){
+				// Hide the address bar!
+				window.scrollTo(0, 1);
+			}, 0);
+		}	
+	});
+
+
 
 //end Jquery
 });
